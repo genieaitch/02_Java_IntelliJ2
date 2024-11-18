5770ea7e1721a73b9d58dda8ed0353026bac4d91
genieaitch <hah4815@naver.com>
2024-11-18 17:25:40 +0900

    자바 스캐너 개요

---
+++ 01_Variable/src/com/variable/practice/Practice1.java
@@ -0,0 +1,10 @@
+package com.variable.practice;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.print("문자 : ");
+
+
+        System.out.println("unicode : ");
+    }
+}
---
+++ 01_Variable/src/com/variable/practice/practice3.java
@@ -0,0 +1,6 @@
+package com.variable.practice;
+
+public class practice3 {
+    public static void main(String[] args) {
+    }
+}
