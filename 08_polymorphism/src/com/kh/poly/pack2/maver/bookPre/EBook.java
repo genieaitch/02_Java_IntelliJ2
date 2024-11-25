07e829ac590cb4aa3226bd11cf1101d12246d90d
genieaitch <hah4815@naver.com>
2024-11-25 17:49:43 +0900

    상속 실습

--- 08_polymorphism/src/com/kh/poly/pack2/maver/bookPre/EBook.java
+++ 08_polymorphism/src/com/kh/poly/pack2/maver/bookPre/EBook.java
@@ -1,4 +1,29 @@
 package com.kh.poly.pack2.maver.bookPre;
 
-public class EBook {
+public class EBook extends Book {
+    private String fileSize;
+
+    public EBook(){
+        super();
+    }
+
+    public EBook(String title, String author, int price, String fileSize) {
+        super(title, author, price);
+        this.fileSize = fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    @Override
+    public String toString() {
+        return "EBook{" + super.toString()+
+                "fileSize='" + fileSize + '\'' +
+                '}';
+    }
 }
--- 08_polymorphism/src/com/kh/poly/pack2/maver/bookPre/PrintBook.java
+++ 08_polymorphism/src/com/kh/poly/pack2/maver/bookPre/PrintBook.java
@@ -1,4 +1,29 @@
 package com.kh.poly.pack2.maver.bookPre;
 
-public class PrintBook {
+public class PrintBook extends Book {
+    private int shippingCost;
+
+    public PrintBook(){
+        super();
+    }
+
+    public PrintBook(String title, String author, int price, int shippingCost) {
+        super(title, author, price);
+        this.shippingCost = shippingCost;
+    }
+
+    public void setShippingCost(int shippingCost) {
+        this.shippingCost = shippingCost;
+    }
+
+    public int getShippingCost() {
+        return shippingCost;
+    }
+
+    @Override
+    public String toString() {
+        return "PrintBook{" + super.toString() +
+                "shippingCost=" + shippingCost +
+                '}';
+    }
 }
